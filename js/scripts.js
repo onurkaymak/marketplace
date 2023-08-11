b513af203a11fff146590de1dca2b53f0ad15cdd
onurkaymak <onurkaymak@outlook.com>
2023-08-10 23:41:41 -0700

    create event listener, listen on input changes to manipulate carousel

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,15 @@ function carouselDefiner(result) {
         const model2 = document.getElementById("model-2").setAttribute('class', 'carousel-item');
     }
 };
+
+window.addEventListener("load", function () {
+    const model1 = document.getElementById("model-1");
+    const model2 = document.getElementById("model-2");
+    const model3 = document.getElementById("model-3");
+
+    const modelSelector = document.getElementById("model_selector");
+    modelSelector.addEventListener("change", function (event) {
+        let result = event.target.value;
+        carouselDefiner(result);
+    });
+});
